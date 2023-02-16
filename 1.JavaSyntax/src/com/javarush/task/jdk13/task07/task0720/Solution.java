0bdaa4a5affc9f77fe436fd92e75f1f896d7c470
Stanislav Sidoryak <stanislav.sidoryak@cma.se>
2023-02-16 11:33:19 +0700

    task0720

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task07/task0720/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.jdk13.task07.task0720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Перестановочка подоспела
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+        int n = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+        //заполним ассив n строк
+        for (int i = 0; i < n; i++) {
+            list.add(reader.readLine());
+        }
+        for (int i = 0; i < m; i++) {
+            list.add(list.size(), list.get(i));
+        }
+        for (int i = 0; i < m; i++) {
+            list.remove(0);
+        }
+        for (String str : list
+        ) {
+            System.out.println(str);
+        }
+
+
+    }
+}
