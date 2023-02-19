4122e1ab1cfa22b0c488c8a6bb91d4eb35e15063
Stanislav Sidoryak <stanislav.sidoryak@cma.se>
2023-02-19 19:58:38 +0700

    task0728

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task08/task0801/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.jdk13.task08.task0801;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Set из растений
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Set<String> set = new HashSet<String>();
+        set.add("арбуз");
+        set.add("банан");
+        set.add("вишня");
+        set.add("груша");
+        set.add("дыня");
+        set.add("ежевика");
+        set.add("женьшень");
+        set.add("земляника");
+        set.add("ирис");
+        set.add("картофель");
+
+        //print set
+        for (String str: set
+             ) {
+            System.out.println(str);
+        }
+
+    }
+}
