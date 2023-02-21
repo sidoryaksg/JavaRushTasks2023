89d1783127e9e397b10e09307c82611aa0d5bea4
Stanislav Sidoryak <stanislav.sidoryak@cma.se>
2023-02-21 09:34:27 +0700

    task0809

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task08/task0809/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.jdk13.task08.task0809;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Измерить сколько времени занимает 10 тысяч вставок для каждого списка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getInsertTimeInMs(new ArrayList()));
+        System.out.println(getInsertTimeInMs(new LinkedList()));
+    }
+
+    public static long getInsertTimeInMs(List list) {
+        // напишите тут ваш код
+        Long date = System.currentTimeMillis();
+
+        insert10000(list);
+        return System.currentTimeMillis() - date;
+
+        // напишите тут ваш код
+
+    }
+
+    public static void insert10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(0, new Object());
+        }
+    }
+}
