35c3517a7cae0c4fda64f5d9d1299c78247582e2
Stanislav Sidoryak <stanislav.sidoryak@cma.se>
2023-02-24 13:20:04 +0700

    task0813

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task08/task0813/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.jdk13.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //напишите тут ваш код
+        Set<String> set = new HashSet<String>();
+        set.add("Люстра");
+        set.add("Люстрация");
+        set.add("Лампа");
+        set.add("Лобстер");
+        set.add("Лосось");
+        set.add("Любовь");
+        set.add("Лак");
+        set.add("Лакмус");
+        set.add("Лев");
+        set.add("Лист");
+        set.add("Лава");
+        set.add("Лавр");
+        set.add("Ложка");
+        set.add("Люк");
+        set.add("Луженый");
+        set.add("Лира");
+        set.add("Лари");
+        set.add("Лифт");
+        set.add("Лажа");
+        set.add("Лях");
+
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
