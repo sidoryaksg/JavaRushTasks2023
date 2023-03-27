d15c2ca6a024136304097a429fab1c0c76e50aa1
sidoriak <sidoryak@gmail.com>
2023-03-27 15:43:30 +0400

    tasks 1204-1208

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваши методы
+        print(10);
+        print(new Integer (11));
+
+    }
+
+    //Напишите тут ваши методы
+    static void print(int val){
+        //System.out.println(val);
+    }
+    static void print(Integer val){
+        //System.out.println(val);
+    }
+}
---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(String s) {
+        System.out.println(s);
+    }
+    public static void print(int i) {
+        System.out.println(i);
+    }
+    public static void print(long l) {
+        System.out.println(l);
+    }
+    public static void print(double d) {
+        System.out.println(d);
+    }
+    public static void print(char c) {
+        System.out.println(c);
+    }
+}
