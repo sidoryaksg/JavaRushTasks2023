660a6e712b13536f7f38f7ce79987cabbe25c6e4
sidoriak <sidoryak@gmail.com>
2023-03-29 20:59:12 +0400

    MinesweeperGame #4

--- JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -1,12 +1,25 @@
 package com.javarush.games.minesweeper;
+
 import com.javarush.engine.cell.*;
+
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
-        super.initialize();
+        createGame();
     }
 
+    private void createGame() {
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                gameField[y][x] = new GameObject(x, y);
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
+    }
 }
+
+
