46dfbb87269e5de196eb3a12a3cb6a4184cff2cb
sidoriak <sidoryak@gmail.com>
2023-04-12 21:22:35 +0400

    Task 1419

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task14.task1419;
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 
 /* 
@@ -24,9 +25,58 @@ public class Solution {
         try {
             float i = 1 / 0;
 
-        } catch (Exception e) {
+        } catch (ArithmeticException e) {
             exceptions.add(e);
         }
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new IndexOutOfBoundsException();
+
+        } catch (IndexOutOfBoundsException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new ConcurrentModificationException();
+        } catch (ConcurrentModificationException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new IOException();
+        } catch (IOException e) {
+            exceptions.add(e);
+        }
+
+        //
+        try {
+            throw new InterruptedIOException() ;
+        } catch (InterruptedIOException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new ClassCastException();
+        } catch (ClassCastException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new ClassNotFoundException();
+        } catch (ClassNotFoundException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new InterruptedException();
+        } catch (InterruptedException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new IllegalArgumentException() ;
+        } catch (IllegalArgumentException e) {
+            exceptions.add(e);
+        }
+
 
         //напишите тут ваш код
 
