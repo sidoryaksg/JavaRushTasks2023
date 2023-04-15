9daeebf28adc07ed7fb9d38a35a098fcc1b5ede0
sidoriak <sidoryak@gmail.com>
2023-04-15 20:45:22 +0400

    Task 1523

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution(Integer value) {
+    }
+    private Solution(String value){
+
+    }
+    Solution(Number value){
+
+    }
+    protected Solution(String value, Integer value1, Integer value2, Integer value3, Integer value4){}
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+
+    public SubSolution(Integer value) {
+        super(value);
+    }
+
+    SubSolution(Number value) {
+        super(value);
+    }
+
+    protected SubSolution(String value, Integer value1, Integer value2, Integer value3, Integer value4) {
+        super(value, value1, value2, value3, value4);
+    }
+}
